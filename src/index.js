00910d43cae66637029e23182424c5c9b0c5fe8c
migo1 <ertmigot@gmail.com>
2023-06-19 15:22:32 +0300

    setup-html-minimum-styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+import leaderboardItems from "./module/dummyData.js";
+
+const leaderboard = document.querySelector("#leaderboard-list");
+
+leaderboardItems.forEach((item) => {
+  const li = document.createElement("li");
+  li.classList.add("leaderboard-item");
+  if (item.id % 2 === 0) {
+    li.classList.add("darker");
+  }
+  li.innerHTML = `${item.name} : ${item.score}`;
+  leaderboard.appendChild(li);
+});
